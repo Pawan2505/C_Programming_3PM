f8fea507461f456f562f41fc044324c48c548550
pawan2505 <pawanaktu2gmail.com>
2026-01-05 12:10:12 +0530

    insertion sort

--- CPP/intro.cpp
+++ CPP/intro.cpp
@@ -3705,32 +3705,98 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+
+
+// void bubbleSort(int arr[], int n){
+
+//     for(int i = 0; i <n ; i++){  // phase
+
+//         for(int j = 0; j < n-i-1; j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//             }
+//         }
+//     }
+// }
+
+// int main(){
+//     int n = 5;
+
+//     int arr[n] = {3,1,8,4,2};
+
+//     bubbleSort(arr,n);
+
+//     for(int i = 0; i < n; i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// void bubbleSort(int arr[], int n){
+
+//     for(int i = 0; i < n; i++){  // phase
+
+//         for(int j = 0; j < n-i-1; j++){ // n-i-1
+
+//             if(arr[j] > arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//             }
+
+//         }
+
+//     }
+// }
+
+// int main(){
+
+//     int n = 5;
+
+//     int arr[n] = {7,18,45,10,9};
+
+//     bubbleSort(arr,n);
+
+//     for(int i = 0; i < n; i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+
+// }
+
+
+
 #include<iostream>
+
 using namespace std;
 
+void insertionSort(int arr[], int n){
 
-void bubbleSort(int arr[], int n){
+    for(int i = 1; i < n; i++){
 
-    for(int i = 0; i <n ; i++){  // phase
+        int current = arr[i];
+        int prev = i-1;
 
-        for(int j = 0; j < n-i-1; j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
-            }
+        while(prev>=0 && arr[prev]>current){
+            arr[prev+1] = arr[prev];
+            prev--;
         }
+        arr[prev+1] = current;
     }
+
 }
 
 int main(){
-    int n = 5;
+int n = 5;
 
-    int arr[n] = {3,1,8,4,2};
+int arr[n] = {6,9,1,8,7};
+insertionSort(arr,n);
 
-    bubbleSort(arr,n);
-
-    for(int i = 0; i < n; i++){
-        cout<<arr[i]<<" ";
-    }
+for(int i = 0; i <n; i++){
+    cout<<arr[i]<<" ";
 }
 
-
+}
